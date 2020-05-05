b665da4287ab363b3a4b35f44e9ab1562fa62dae
aginies <aginies@suse.com>
2020-05-05 14:40:09 +0200

    update the menu

--- chaossource/chaos.c
+++ chaossource/chaos.c
@@ -1,5 +1,6 @@
 #include "g_local.h"
 #include "stdlog.h"
+#include "chaos.h"
 
 
 
@@ -45,14 +46,16 @@ void Chaos_SetStats(edict_t *self) {
     edict_t *ent = self;
     short *stats = self->client->ps.stats;
 
+    if(!level.intermissiontime) {
     //  already used
-    // 1 2 4 6 7 9 11 25 27 30
+    // 1 2 4 6 7 9 11 25 26 27 30
+    // Don't use index > 31 this will cause error
     stats[10] = ent->client->pers.inventory[ITEM_INDEX(FindItem("shells"))];
     stats[29] = ent->client->pers.inventory[ITEM_INDEX(FindItem("explosive shells"))];
     stats[12] = ent->client->pers.inventory[ITEM_INDEX(FindItem("slugs"))];
     stats[27] = ent->client->pers.inventory[ITEM_INDEX(FindItem("buzzes"))];
 
-    stats[26] = ent->client->pers.inventory[ITEM_INDEX(FindItem("grenades"))];
+    stats[31] = ent->client->pers.inventory[ITEM_INDEX(FindItem("grenades"))];
     stats[15] = ent->client->pers.inventory[ITEM_INDEX(FindItem("flash grenades"))];
     stats[16] = ent->client->pers.inventory[ITEM_INDEX(FindItem("poison grenades"))];
     stats[17] = ent->client->pers.inventory[ITEM_INDEX(FindItem("Laser Mines"))];
@@ -67,8 +70,9 @@ void Chaos_SetStats(edict_t *self) {
 
     stats[24] = ent->client->pers.inventory[ITEM_INDEX(FindItem("cells"))];
 
-//    stats[27] = ent->client->pers.inventory[ITEM_INDEX(FindItem("nuke"))];
-//    stats[28] = ent->client->pers.inventory[ITEM_INDEX(FindItem("gravity vortex"))];
+    stats[28] = ent->client->pers.inventory[ITEM_INDEX(FindItem("nuke"))];
+    stats[13] = ent->client->pers.inventory[ITEM_INDEX(FindItem("gravity vortex"))];
+    }
 
     if(ctf->value)
         SetCTFStats(ent, stats);
@@ -77,37 +81,35 @@ void Chaos_SetStats(edict_t *self) {
 //===============================
 // statusbar
 
-void StatusBar_UpdateAll(void) {
-    int i;
-    edict_t *ent;
-
-    for(i = 0; i < game.maxclients; i++) {
-        ent = g_edicts + 1 + i;
-        if(!ent->inuse)
-            continue;
-        return;
-    }
-}
-
 char *ammobar =
+"yb -156 xr -24 picn a_xgrenades xr -74 num 3 18 "
 "yb -122 xr -24 picn a_lmines xr -74 num 3 17 "
-"yb -98 xr -24 picn a_grenades1 xr -74 num 3 26 "
+"yb -98 xr -24 picn a_grenades1 xr -74 num 3 31 "
 "yb -74 xr -24 picn a_fgrenades xr -74 num 3 15 "
 "yb -50 xr -24 picn a_pgrenades xr -74 num 3 16 "
-"yb -26 xr -24 picn a_xgrenades xr -74 num 3 18 "
 "yt 84 xr -24 picn a_eshells xr -74 num 3 29 "
 "yt 108 xr -24 picn a_shells xr -74 num 3 10 "
 "yt 156 xr -24 picn a_slugs xr -74 num 3 12 "
 "yt 180 xr -24 picn a_buzz xr -74 num 3 27 "
 "yb -74 xl 0 picn a_arrows xl 24 num 3 19 "
 "yb -50 xl 0 picn a_parrows xl 24 num 3 20 "
-"yb -26 xl 0 picn a_earrows xl 24 num 3 21 "
+"yb -98 xl 0 picn a_earrows xl 24 num 3 21 "
 "yt 136 xl 0 picn a_rockets xl 24 num 3 22 "
 "yt 160 xl 0 picn a_grockets xl 24 num 3 23 "
 "yt 208 xl 0 picn a_cells xl 24 num 3 24 "
 ;
 //"yt 232 xl 0 picn a_nuke xl 24 num 3 27"
 //"yt 256 xl 0 picn a_vortex xl 24 num 3 28"
+
+char *nukevortex = 
+"yt 112 xl 0 picn a_nuke xl 24 num 3 28 "
+"yt 88 xl 0 picn a_vortex xl 24 num 3 13 "
+;
+
+char *fragsbar =
+//  frags
+"yt 40 xl 0 picn p_kamikaze xl 24 num 3 14 "
+;
 
 char *chaos_statusbar =
 "yb -24 "
@@ -138,10 +140,6 @@ char *chaos_statusbar =
 //  help / weapon icon 
 "if 11 xv 148 pic 11 endif "
 
-//  frags
-//"yt 136 xl 0 picn a_rockets xl 24 num 3 22 "
-"yt 40 xl 0 picn p_kamikaze xl 24 num 3 14 "
-
 //tech
 //"yt 120 if 26 xr -26 pic 26 endif "
 
@@ -151,13 +149,28 @@ char *chaos_statusbar =
 
 
 int StatusBar_Update(edict_t *ent) {
-    char statusbar[1400] = "";
+    char statusbar[2400];
 
+//    if(!(ent->chaos_flags & CHAOS_STATUSBAR))
+//        return 0;
+
+    ent->chaos_flags &= ~CHAOS_STATUSBAR;
+
+    if (strcmp(ent->classname, "player") == 0)
+    {
     if(!ctf->value) 
     {
 	    if (!ent->client->showammo)
 	    {
         	strlcat(statusbar, ammobar, sizeof(statusbar));
+	    }
+	    if (ent->client->showfrags)
+	    {
+        	strlcat(statusbar, fragsbar, sizeof(statusbar));
+	    }
+	    if (ent->client->shownv)
+	    {
+        	strlcat(statusbar, nukevortex, sizeof(statusbar));
 	    }
         strlcat(statusbar, chaos_statusbar, sizeof(statusbar));
     }
@@ -166,24 +179,31 @@ int StatusBar_Update(edict_t *ent) {
 	    {
         	strlcat(statusbar, ammobar, sizeof(statusbar));
 	    }
+	    if (ent->client->showfrags)
+	    {
+        	strlcat(statusbar, fragsbar, sizeof(statusbar));
+	    }
+	    if (ent->client->shownv)
+	    {
+        	strlcat(statusbar, nukevortex, sizeof(statusbar));
+	    }
         strlcat(statusbar, ctf_statusbar, sizeof(statusbar));
     }
 
     gi.configstring (CS_STATUSBAR, statusbar);
-
     gi.WriteByte(0x0D);
     gi.WriteShort(5);
+//    gi.WriteShort (ent-g_edicts+10);
     gi.WriteString(statusbar);
     gi.unicast(ent, false);
     return strlen(statusbar);
+    }
+    return 0;
 }
 
+//
 // MENU
-
-void ChaosOpenMenu(edict_t *ent);
-void ChaosCredits(edict_t *ent, pmenu_t *p);
-void MenuChangeVar(edict_t *ent, pmenu_t *p);
-void MenuHelp(edict_t *ent, pmenu_t *p);
+//
 
 void ChaosReturnToMain(edict_t *ent, pmenu_t *p)
 {
@@ -191,24 +211,27 @@ void ChaosReturnToMain(edict_t *ent, pmenu_t *p)
         ChaosOpenMenu(ent);
 }
 
-
 pmenu_t ccreditsmenu[] = {
-        { "*Chaos Deathmatch v1.16",                            PMENU_ALIGN_CENTER, NULL, NULL },
-        { NULL,                                                         PMENU_ALIGN_CENTER, NULL, NULL },
-        { "*Programming",                                       PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Flash (flash@telefragged.com)",      PMENU_ALIGN_CENTER, NULL, NULL },
-        { "*Current devel",                                       PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Guibo (guibo@guibo.com)",      PMENU_ALIGN_CENTER, NULL, NULL },
-        { "*Level Design",                                      PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Nat (nnp@greennet.net)",                     PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Craig (car188@psu.edu)",                     PMENU_ALIGN_CENTER, NULL, NULL },
-        { "*Art",                                                       PMENU_ALIGN_CENTER, NULL, NULL },
-        { "SPA (spa@telefragged.com)",          PMENU_ALIGN_CENTER, NULL, NULL },
-        { "*Sound",                                                     PMENU_ALIGN_CENTER, NULL, NULL },
-        { "SPA (spa@telefragged.com)",          PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Nat (nnp@greennet.net)",                     PMENU_ALIGN_CENTER, NULL, NULL },
-        { NULL,                                                         PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Return to Main Menu",                        PMENU_ALIGN_LEFT, NULL, ChaosReturnToMain }
+        { "*Chaos Deathmatch",PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "*Programming",PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Flash (flash@telefragged.com)",PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { "*Current devel",PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Guibo (guibo@guibo.com)",PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { "*Level Design",PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Nat (nnp@greennet.net)",PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Craig (car188@psu.edu)",PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { "*Art",PMENU_ALIGN_CENTER, NULL, NULL },
+        { "SPA (spa@telefragged.com)",PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { "*Sound",PMENU_ALIGN_CENTER, NULL, NULL },
+        { "SPA (spa@telefragged.com)",PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Nat (nnp@greennet.net)",PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Retourner Menu Principal",PMENU_ALIGN_LEFT, NULL, ChaosReturnToMain }
 };
 
 void ChaosCredits(edict_t *ent, pmenu_t *p)
@@ -217,52 +240,103 @@ void ChaosCredits(edict_t *ent, pmenu_t *p)
         PMenu_Open(ent, ccreditsmenu, -1, sizeof(ccreditsmenu) / sizeof(pmenu_t));
 }
 
-pmenu_t changevarmenu[] = {
-        { "Change some var",                            PMENU_ALIGN_CENTER, NULL, NULL },
-        { NULL,                                                         PMENU_ALIGN_CENTER, NULL, NULL },
-        { NULL,                                                         PMENU_ALIGN_CENTER, NULL, NULL },
-        { NULL,                                                         PMENU_ALIGN_CENTER, NULL, NULL },
-        { NULL,                                                         PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Return to Main Menu",                        PMENU_ALIGN_LEFT, NULL, ChaosReturnToMain }
+pmenu_t interfacemenu[] = {
+        { "Interface",PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Status Munitions",PMENU_ALIGN_LEFT, NULL, ShowAmmoMenu },
+        { "[ENTER] Pour changer",PMENU_ALIGN_RIGHT, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Nuke / Vortex",PMENU_ALIGN_LEFT, NULL, ShowNVMenu },
+        { "[ENTER] Pour changer",PMENU_ALIGN_RIGHT, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Montrer Nombre Frag(s)",PMENU_ALIGN_LEFT, NULL, ShowFragsMenu },
+        { "[ENTER] Pour changer",PMENU_ALIGN_RIGHT, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Retourner Menu Principal",PMENU_ALIGN_LEFT, NULL, ChaosReturnToMain }
 };
 
 pmenu_t helpmenu[] = {
-        { "*HELP",                            PMENU_ALIGN_CENTER, NULL, NULL },
-        { NULL,                                                         PMENU_ALIGN_CENTER, NULL, NULL },
-        { "*showammo",                        PMENU_ALIGN_LEFT, NULL, NULL },
-        { "Show Ammos (on/off)",                        PMENU_ALIGN_RIGHT, NULL, NULL },
-        { NULL,                                                         PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Jedi force:",                                                      PMENU_ALIGN_CENTER, NULL, NULL },
-        { "-----------",                                                      PMENU_ALIGN_CENTER, NULL, NULL },
-        { "*pull",                        PMENU_ALIGN_LEFT, NULL, NULL },
-        { "Jedi force to pull enemy",                        PMENU_ALIGN_RIGHT, NULL, NULL },
-        { "*push",                        PMENU_ALIGN_LEFT, NULL, NULL },
-        { "Jedi force to push enemy",                        PMENU_ALIGN_RIGHT, NULL, NULL },
-        { "*fkill",                        PMENU_ALIGN_LEFT, NULL, NULL },
-        { "Darth Vader force!",                        PMENU_ALIGN_RIGHT, NULL, NULL },
-        { NULL,                                                         PMENU_ALIGN_CENTER, NULL, NULL },
-        { NULL,                                                         PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Return to Main Menu",                        PMENU_ALIGN_LEFT, NULL, ChaosReturnToMain }
+        { "*Commande de Console",PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "*showammo",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "Status Munitions",PMENU_ALIGN_LEFT, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "*shownv",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "Status Nuke et Vortex",PMENU_ALIGN_LEFT, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "*showfrags",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "Nombre de Frag(s)",PMENU_ALIGN_LEFT, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "*pull",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "Force pour Pousser ",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "*push",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "Force pour Attirer",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "*fkill",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "DarK Vador Force!",PMENU_ALIGN_LEFT, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "*observer",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "Camera libre",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "F7 F8 quitter camera",PMENU_ALIGN_RIGHT, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "*Dans votre Configuration",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "bind MOUSE4 ''fkill''",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "bind MOUSE5 ''pull''",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "bind F12 ''showammo''",PMENU_ALIGN_LEFT, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Retourner Menu Principal",PMENU_ALIGN_LEFT, NULL, ChaosReturnToMain }
 };
 
 
 pmenu_t mainmenu[] = {
-        { "*Chaos Deathmatch",  PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Version 1.16",       PMENU_ALIGN_RIGHT, NULL, NULL },
-        { NULL,                                 PMENU_ALIGN_CENTER, NULL, NULL },
-        { NULL,                                 PMENU_ALIGN_CENTER, NULL, NULL },
-        { NULL,                                 PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Help",                    PMENU_ALIGN_LEFT, NULL, MenuHelp },
-        { "Change Var",                    PMENU_ALIGN_LEFT, NULL, MenuChangeVar },
-        { NULL,                                 PMENU_ALIGN_CENTER, NULL, NULL },
-        { "Credits",                    PMENU_ALIGN_LEFT, NULL, ChaosCredits },
-        { NULL,                                 PMENU_ALIGN_LEFT, NULL, NULL },
-        { "Use [ and ] to move cursor", PMENU_ALIGN_LEFT, NULL, NULL },
-        { "ENTER to select",    PMENU_ALIGN_LEFT, NULL, NULL },
-        { "ESC to Exit Menu",   PMENU_ALIGN_LEFT, NULL, NULL },
-        { "(TAB to Return)",    PMENU_ALIGN_LEFT, NULL, NULL },
-        { NULL,                                 PMENU_ALIGN_LEFT, NULL, NULL },
+        { "*Chaos Deathmatch",PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Devel Version 1.16",PMENU_ALIGN_RIGHT, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "* ! VERSION XP !",PMENU_ALIGN_CENTER, NULL, NULL },
+        { " ! Reportez moi les Bugs !",PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Afficher ce menu: menu",PMENU_ALIGN_LEFT, NULL, NULL },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { "*Bots",PMENU_ALIGN_LEFT, NULL,MenuBots },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "*Interface",PMENU_ALIGN_LEFT, NULL, MenuInterface },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "*Aide",PMENU_ALIGN_LEFT, NULL, MenuHelp },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "*Credits",PMENU_ALIGN_LEFT, NULL, ChaosCredits },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { "[^] et [$] pour le curseur", PMENU_ALIGN_LEFT, NULL, NULL },
+        { "[ENTER] Selection",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "[ESC] Sortir",PMENU_ALIGN_LEFT, NULL, NULL },
 };
+
+pmenu_t botsmenu[] = {
+        { "*Bots Administration",PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "*Sur serveur distant",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "*Password Obligatoire!",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "Exemple:",PMENU_ALIGN_LEFT, NULL, NULL },
+        { "seta rconpassword ''PASS''",PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { "Ajouter un Bot (niveau 1)",PMENU_ALIGN_LEFT, NULL, AddBotsCmd },
+        { "Ajouter un Bot (niveau 2)",PMENU_ALIGN_LEFT, NULL, AddBotsCmd2 },
+        { NULL,PMENU_ALIGN_CENTER, NULL, NULL },
+        { "Retirer Tous les bots",PMENU_ALIGN_LEFT, NULL, KillBotsCmd },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { NULL,PMENU_ALIGN_LEFT, NULL, NULL },
+        { "Retourner Menu Principal",PMENU_ALIGN_LEFT, NULL, ChaosReturnToMain }
+};
+
 
 
 void ChaosOpenMenu(edict_t *ent)
@@ -270,10 +344,10 @@ void ChaosOpenMenu(edict_t *ent)
         PMenu_Open(ent, mainmenu, 1, sizeof(mainmenu) / sizeof(pmenu_t));
 }
 
-void MenuChangeVar(edict_t *ent, pmenu_t *p)
+void MenuInterface(edict_t *ent, pmenu_t *p)
 {
         PMenu_Close(ent);
-        PMenu_Open(ent, changevarmenu, -1, sizeof(changevarmenu) / sizeof(pmenu_t));
+        PMenu_Open(ent, interfacemenu, -1, sizeof(interfacemenu) / sizeof(pmenu_t));
 }
 
 void MenuHelp(edict_t *ent, pmenu_t *p)
@@ -282,4 +356,38 @@ void MenuHelp(edict_t *ent, pmenu_t *p)
         PMenu_Open(ent, helpmenu, -1, sizeof(helpmenu) / sizeof(pmenu_t));
 }
 
+void MenuBots(edict_t *ent, pmenu_t *p)
+{
+        PMenu_Close(ent);
+        PMenu_Open(ent, botsmenu, -1, sizeof(botsmenu) / sizeof(pmenu_t));
+}
+
+void KillBotsCmd (edict_t *ent, pmenu_t *p)
+{
+    stuffcmd(ent, "rcon sv killbot all\n");
+}
+
+void AddBotsCmd (edict_t *ent, pmenu_t *p)
+{
+    stuffcmd(ent, "rcon sv addbots 1 1\n");
+}
+void AddBotsCmd2 (edict_t *ent, pmenu_t *p)
+{
+    stuffcmd(ent, "rcon sv addbots 1 2\n");
+}
+
+void ShowAmmoMenu (edict_t *ent, pmenu_t *p)
+{
+    stuffcmd(ent, "showammo\n");
+}
+
+void ShowNVMenu (edict_t *ent, pmenu_t *p)
+{
+    stuffcmd(ent, "shownv\n");
+}
+
+void ShowFragsMenu (edict_t *ent, pmenu_t *p)
+{
+    stuffcmd(ent, "showfrags\n");
+}
 
