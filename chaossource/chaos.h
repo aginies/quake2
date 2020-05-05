d22cbc2da2de32213151e3fb6f6b47ab5e083ecd
aginies <aginies@suse.com>
2020-05-05 14:30:11 +0200

    add some more definition

--- chaossource/chaos.h
+++ chaossource/chaos.h
@@ -1,6 +1,8 @@
 //
 //
 
+#define CHAOS_STATUSBAR 1
+#define CHAOS_MAINMENU 2
 
 qboolean Observer(edict_t *ent, qboolean check);
 
@@ -8,3 +10,17 @@ qboolean Observer(edict_t *ent, qboolean check);
 int StatusBar_Update(edict_t *ent);
 void StatusBar_UpdateAll(void);
 void Chaos_SetStats(edict_t *ent);
+
+// MENU
+void ChaosOpenMenu(edict_t *ent);
+void ChaosCredits(edict_t *ent, pmenu_t *p);
+void MenuInterface(edict_t *ent, pmenu_t *p);
+void MenuHelp(edict_t *ent, pmenu_t *p);
+void MenuBots(edict_t *ent, pmenu_t *p);
+void KillBotsCmd (edict_t *ent, pmenu_t *p);
+void AddBotsCmd (edict_t *ent, pmenu_t *p);
+void AddBotsCmd2 (edict_t *ent, pmenu_t *p);
+void ShowAmmoMenu (edict_t *ent, pmenu_t *p);
+void ShowNVMenu (edict_t *ent, pmenu_t *p);
+void ShowFragsMenu (edict_t *ent, pmenu_t *p);
+
